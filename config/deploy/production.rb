e1c8636ac405698e7961fb8a61cd7f94f4ce7e4e
Aniket Rao <aniket.rao@myridz.com>
2016-06-16 09:56:04 +0530

    shortened puma socket name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,7 +10,7 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/version_1/production/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/chd-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
