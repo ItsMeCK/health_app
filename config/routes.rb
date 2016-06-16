50081da3c3f283b5f6d63b9e68f21c7d6c05fc5a
Aniket Rao <aniket.rao@myridz.com>
2016-06-16 08:45:59 +0530

    Added User show and create API endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # API Definition
-  namespace :web, defaults: { format: :json }, path: '/' do
+  namespace :web, defaults: { format: :json }, path: '/web/' do
     scope module: :v1 do
     # We are going to list our resources here
+    	resources :users, :only => [:create, :show]
     end
   end
 end
