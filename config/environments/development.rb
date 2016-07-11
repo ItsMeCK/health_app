d88a2a500166fb1dce1236bb4ea865a4317b552e
Aniket Rao <aniket.rao@myridz.com>
2016-07-11 18:25:42 +0530

    added User Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,8 +47,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    user_name: APP_CONFIG['myridz_username'],
-    password: APP_CONFIG['myridz_password'],
+    user_name: "admin@myridz.com",
+    password: "admin@myridz",
     authentication: "plain",
     enable_starttls_auto: true,
     openssl_verify_mode: "none"
