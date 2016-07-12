2ca85bd696334d26ec5107b0c3f59eccfa8e71d6
Aniket Rao <aniket.rao@myridz.com>
2016-07-12 14:32:35 +0530

    association mistype in serializers

--- app/serializers/mobile/v1/profile_serializer.rb
+++ app/serializers/mobile/v1/profile_serializer.rb
@@ -1,5 +1,3 @@
 class Mobile::V1::ProfileSerializer < ActiveModel::Serializer
   attributes :full_name, :mobile, :dob, :email, :gender, :bike_owned, :riding_since, :address, :location, :profession, :bio, :hog_privacy, :profile_image, :user_id
-
-  belongs_to :user
 end
