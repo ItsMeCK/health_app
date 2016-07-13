37a920f93f9afeda0020caff8b1c78ebfd9961b0
Aniket Rao <aniket.rao@myridz.com>
2016-07-13 13:02:53 +0530

    try name

--- app/serializers/web/v1/specification_serializer.rb
+++ app/serializers/web/v1/specification_serializer.rb
@@ -1,9 +1,9 @@
 class Web::V1::SpecificationSerializer < ActiveModel::Serializer
-  attributes :name, :value, :active, :bike_id, :specification_type_id
+  attributes :id, :name, :value, :active, :bike_id, :specification_type_id
 
   def attributes
   	data = super
-  	data[:bike_name] = Bike.find_by_id(data[:bike_id]).name
+  	data[:bike_name] = Bike.find_by_id(data[:bike_id]).try(:name)
   	data[:specification_type] = SpecificationType.find_by_id(data[:specification_type_id]).name
   	data
   end
