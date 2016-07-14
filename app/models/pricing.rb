4f247a8aa6dde43a20ac363277e9916ae65bff77
anil kumar <anil.kumar@myridz.com>
2016-07-14 15:22:34 +0530

    price values for bike

--- app/models/pricing.rb
+++ app/models/pricing.rb
@@ -1,4 +1,17 @@
 class Pricing < ActiveRecord::Base
 	belongs_to :bike
 	belongs_to :price_field
+
+
+	def price_field_type
+		 PriceField.find_by_id(self.price_field_id).name
+	end
+
+	def bike_type
+	    Bike.find_by_id(self.bike_id).name
+	end
+
+	def as_json(options={})
+		super.as_json(options).merge({ :bike_name => bike_type, :price_field_type => price_field_type })
+	end
 end
