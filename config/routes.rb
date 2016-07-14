5cdb4731705ef21d9d9df0369589347dc30dc4ba
anil kumar <anil.kumar@myridz.com>
2016-07-14 13:12:07 +0530

    scaffold controller price field with routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
       resources :booking_time_controls
       resources :key_feature_types
       resources :key_features
+      resources :price_fields
     end
   end
 
