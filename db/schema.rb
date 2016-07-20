582d8f06c768baa98e20bc3d4e8e9d4e343c1e84
anil kumar <anil.kumar@myridz.com>
2016-07-20 16:22:54 +0530

    modified accessory parameters

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720102210) do
+ActiveRecord::Schema.define(version: 20160720104626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20160720102210) do
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
     t.integer  "accessory_category_id"
+    t.string   "part_number"
+    t.integer  "size"
+    t.string   "price"
   end
 
   create_table "accessory_categories", force: :cascade do |t|
