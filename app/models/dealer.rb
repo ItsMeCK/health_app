40bf503ba9f1cd0d6c8c9938436fbfc39dfa0e40
anil kumar <anil.kumar@myridz.com>
2016-07-20 17:30:34 +0530

    adding the dealer type attribute to dealer

--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -5,6 +5,15 @@ class Dealer < ActiveRecord::Base
 	#call back
 	after_create :create_joint_table
 
+	
+	def dealer_tyep_name
+		DealerType.find(self.dealer_type_id).dealer_type
+	end
+
+	def as_json(options={})
+		super.as_json(options).merge({:dealer_type => dealer_tyep_name})
+	end
+
 	def create_joint_table
 		@dealer_type = DealerType.find(self.dealer_type_id)
 		self.dealer_types << @dealer_type
