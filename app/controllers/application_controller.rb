6c253e3006d52e066d76df425e03c3f008cb0ab1
anik3tra0 <anik3tra0@gmail.com>
2016-07-26 10:19:03 +0530

    Changed CORS Policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::API
 	# include ::ActionController::Serialization
 	# include ActiveModel::Serialization
-	after_action :set_access_control_headers
+	# after_action :set_access_control_headers
 	
 	def default_serializer_options
     {module_namespace: self.class.name.deconstantize}
   end
 
-private
+# private
 
- def set_access_control_headers
-   headers['Access-Control-Allow-Origin'] = "*"
-   headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
- end
+ # def set_access_control_headers
+ #   headers['Access-Control-Allow-Origin'] = "*"
+ #   headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+ # end
 
 end
