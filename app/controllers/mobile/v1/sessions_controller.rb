37f9ca3ec61f9dea815f070cadfbc8ba709faa55
Aniket Rao <aniket.rao@myridz.com>
2016-08-02 12:52:24 +0530

    Removed reset authentication from create

--- app/controllers/mobile/v1/sessions_controller.rb
+++ app/controllers/mobile/v1/sessions_controller.rb
@@ -8,7 +8,6 @@ class Mobile::V1::SessionsController < ApplicationController
     if user.present?
       if user.valid_password? user_password
         sign_in user, store: false
-        user.reset_authentication_token!
         user.save
         render json: user, status: 200, location: [:mobile, user]
       else
