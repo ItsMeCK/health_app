d32cef54934f38c5f5b65685fb90f82a286ce164
anil kumar <anil.kumar@myridz.com>
2016-08-03 16:01:35 +0530

    contact tyep name to contact numbers

--- app/models/contact_number.rb
+++ app/models/contact_number.rb
@@ -1,3 +1,12 @@
 class ContactNumber < ActiveRecord::Base
 	belongs_to :contact_type
+
+
+	def contact_type_name
+      ContactType.find(self.contact_type_id).label
+	end
+	
+	def as_json(options={})
+		super.as_json(options).merge({:contact_type_name => contact_type_name})
+	end
 end
