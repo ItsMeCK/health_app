3f25dd699104b5aad9a5138d11106af11e94f803
Aniket Rao <chandrakant.shinde@myridz.com>
2016-08-09 12:20:39 +0530

    Reverted deploy file changs for whenever gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,16 +5,6 @@ set :repo_url,        'git@bitbucket.org:myridz/coromandel-harley-davidson.git'
 set :application,     'chd'
 set :user,            'deploy'
 
-# Whenevr setup
-after "deploy:symlink", "deploy:update_crontab"
-
-namespace :deploy do
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application}"
-  end
-end
-
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
