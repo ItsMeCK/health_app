18aecb16083c33ee37a99be424e66d8c1cb1ff16
Aniket Rao <chandrakant.shinde@myridz.com>
2016-08-10 14:57:13 +0530

    notification Serialiser modified

--- app/serializers/web/v1/notification_serializer.rb
+++ app/serializers/web/v1/notification_serializer.rb
@@ -1,3 +1,10 @@
 class NotificationSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, 
+
+  def attributes
+  	data = super
+  	template = Notification.notification_template
+  	data[:title] = template.title
+  	date[:description] = template.content
+
 end
