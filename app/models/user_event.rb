245a2bdd8e5c15d7b50cd6ce5c5e1ba78c45cf6a
anil kumar <anil.kumar@myridz.com>
2016-08-10 16:50:09 +0530

    events data api + key words changes

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -1,4 +1,27 @@
 class UserEvent < ActiveRecord::Base
 	belongs_to :event
 	belongs_to :user
+
+	def event_date
+		self.event.try(:event_date)
+	end
+	def event_time
+		self.event.try(:event_time)
+	end
+	def location
+		self.event.try(:location)
+	end
+	def title
+		self.event.try(:title)
+	end
+	def description
+		self.event.try(:description)
+	end
+	
+
+	def as_json(options={})
+		super_data = super(:only => [:id, :user_id, :event_id, :perticipate_event, :user_event_date])
+		event = merge({:title => title, :event_date => event_date, :event_time => event_time, :assembly_lacation => location, :description => description})
+	 super_data.event
+	end
 end
