eaa07b27d02fa75f2fc6a629e71e974291d7f48d
Chandrakant Shinde <chandrakant.shinde@myridz.com>
2016-08-11 14:24:24 +0530

    Saving token

--- app/controllers/mobile/v1/users_controller.rb
+++ app/controllers/mobile/v1/users_controller.rb
@@ -57,8 +57,7 @@ class Mobile::V1::UsersController < ApplicationController
 	end
 
 	def notification_count
-		logger.info "================#{current_user.inspect}"
-  	@count = current_user.notification_count
+  	@count = current_user.notification_count.count
   	render json: @count
 	end
 
