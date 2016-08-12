b9471133421f0937be2832c89068c194c29ba1e3
Chandrakant Shinde <chandrakant.shinde@myridz.com>
2016-08-12 11:00:16 +0530

    Notification Count fixes

--- app/serializers/notification_count_serializer.rb
+++ app/serializers/notification_count_serializer.rb
@@ -5,7 +5,7 @@ class NotificationCountSerializer < ActiveModel::Serializer
   	data = super
   	n_c = NotificationCount.find data[:id]
   	data[:Offer] = n_c.offer || data[:offer]
-  	data[:Bookings] = n_c.bookings = data[:bookings]
+  	data[:Bookings] = n_c.bookings || data[:bookings]
   	data[:Events] = n_c.events || data[:events]
   	data[:Accessories] = n_c.accessories ||data[:accessories]
   	data
