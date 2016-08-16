28a5af079fb24920ddc81b530c71959b436cd831
anil kumar <anil.kumar@myridz.com>
2016-08-16 12:42:32 +0530

    fixing the all booking issue

--- app/models/service_booking.rb
+++ app/models/service_booking.rb
@@ -2,16 +2,19 @@ class ServiceBooking < ActiveRecord::Base
 	belongs_to :my_bike
 
 	def my_bike_name
-		#if MyBike.exists?(self.my_bike_id)
+		if MyBike.exists?(self.my_bike_id)
 			MyBike.find_by_id(self.my_bike_id).try(:bike) || "null"	
+		end
 	end
 	def user_name
-		#if MyBike.exists?(self.my_bike_id)
+		if MyBike.exists?(self.my_bike_id)
 			MyBike.find_by_id(self.my_bike_id).user.profile.try(:full_name) || "null"	
+		end
 	end
 	def user_mobile
-		#if MyBike.exists?(self.my_bike_id)
+		if MyBike.exists?(self.my_bike_id)
 			MyBike.find_by_id(self.my_bike_id).user.profile.try(:mobile) || "null"	
+		end
 	end
 	
 
