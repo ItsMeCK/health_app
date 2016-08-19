0be228f6e6342f164c97f25420377704d3109bde
anil kumar <anil.kumar@myridz.com>
2016-08-19 14:21:25 +0530

    changed the response of all bookings

--- app/models/service_booking.rb
+++ app/models/service_booking.rb
@@ -16,10 +16,15 @@ class ServiceBooking < ActiveRecord::Base
 			MyBike.find_by_id(self.my_bike_id).user.profile.try(:mobile) || "null"	
 		end
 	end
+	def user_email
+		if MyBike.exists?(self.my_bike_id)
+			MyBike.find_by_id(self.my_bike_id).user.email || "null"	
+		end
+	end
 	
 
 	def as_json(options={})
-		super.as_json(options).merge({:my_bike_name => my_bike_name, :user_name => user_name, :user_mobile => user_mobile})
+		super.as_json(options).merge({:my_bike_name => my_bike_name, :user_name => user_name, :user_mobile => user_mobile, :user_email => user_email})
 	end
 
 end
