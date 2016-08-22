9b5d43fb5083f5f4e29872120f41ab41341a8a06
anil kumar <anil.kumar@myridz.com>
2016-08-22 17:47:49 +0530

    count api for admin dashboard

--- app/controllers/web/v1/test_rides_controller.rb
+++ app/controllers/web/v1/test_rides_controller.rb
@@ -62,7 +62,7 @@ class Web::V1::TestRidesController < ApplicationController
     head :please_try_again!
   end
 
- def all_bookings
+  def all_bookings
    service_bookings = ServiceBooking.where('extract(year  from service_date) = ?', params[:year]).where('extract(month  from service_date) = ?', params[:month])
    test_ride_bookings = TestRide.where('extract(year  from ride_date) = ?',  params[:year]).where('extract(month  from ride_date) = ?',  params[:month]) 
    insurance_bookings = InsuranceRenewal.where('extract(year  from purchase_date) = ?',  params[:year]).where('extract(month  from purchase_date) = ?',  params[:month]) 
@@ -84,7 +84,7 @@ class Web::V1::TestRidesController < ApplicationController
           else
             {contact: bookings }
           end
-          }.flat_map(&:entries).group_by(&:first).map{|k,v| Hash["#{k}_count": v.map(&:last).count, "#{k}": v.map(&:last)  ] }  ]  }
+          }.flat_map(&:entries).group_by(&:first).map{ |k,v| Hash["#{k}_count": v.map(&:last).count, "#{k}": v.map(&:last)  ] }  ]  }
           render json: @all_bookings, :root => "bookings"
         end
 
@@ -110,31 +110,48 @@ class Web::V1::TestRidesController < ApplicationController
           insurance_bookings = InsuranceRenewal.where('extract(year  from purchase_date) = ?',  params[:year]).where('extract(month  from purchase_date) = ?',  params[:month]) 
           service = service_bookings.collect { |service_booking| date = service_booking.service_date.strftime("%d/%m/%Y") 
             Hash[date, service_booking] }
-          test_ride = test_ride_bookings.collect { |test_ride| date = test_ride.ride_date.strftime("%d/%m/%Y")
+            test_ride = test_ride_bookings.collect { |test_ride| date = test_ride.ride_date.strftime("%d/%m/%Y")
               Hash[date, test_ride] }
-          insurance_renewal = insurance_bookings.collect { |insurance_booking| date = insurance_booking.purchase_date.strftime("%d/%m/%Y") 
+              insurance_renewal = insurance_bookings.collect { |insurance_booking| date = insurance_booking.purchase_date.strftime("%d/%m/%Y") 
                 Hash[date, insurance_booking] }
-          all_bookings_array = service + test_ride + insurance_renewal
-          bookings = all_bookings_array.flat_map(&:entries).group_by(&:first)
-          counts = Hash.new(0)
-          @all_bookings = bookings.map{|k,v| Hash[date: k, count: v.map(&:last).count, all_bookings: v.map(&:last).collect{ |bookings| 
-                binding.pry
-                  }#.flat_map(&:entries).group_by(&:first).map{|k,v| Hash["#{k}_count": v.map(&:last).count ] }  
+                all_bookings_array = service + test_ride + insurance_renewal
+                bookings = all_bookings_array.flat_map(&:entries).group_by(&:first)
+                @all_bookings = bookings.map{|k,v| 
+                  @service = []
+                  @test_ride = []
+                  @insurence = []
+                #@fgh = []
+                Hash[date: k, count: v.map(&:last).count, all_bookings: v.map(&:last).collect{ |bookings| 
+                  if bookings.try(:service_date)
+                    @service << bookings
+                    { service_bookings: bookings }                 
+                  elsif bookings.try(:ride_date)
+                    @test_ride << bookings
+                    { test_ride_bookings: bookings }           
+                  elsif bookings.try(:purchase_date)
+                    @insurence << bookings
+                    { insurence_renewal_bookings: bookings }         
+                  else
+                    {contact: bookings }               
+                  end
+                  }.flat_map(&:entries).group_by(&:first).map{ |k,v| 
+                    Hash[service_booking_count: @service.count, test_ride_bookingcount: @test_ride.count, insurence_booking_count: @insurence.count]}.uniq 
                   ] 
                   
-                   }
-                 
-                  render json: @all_bookings, :root => "bookings"
-        end
+                }
+                user = User.where(role: "guest").count
+                @count = {all_booking: @all_bookings, users_count: user }
+                render json: @count, :root => "bookings_users"
+              end
 
-        private
+              private
 
-        def set_test_ride
-          @test_ride = TestRide.find(params[:id])
-        end
+              def set_test_ride
+                @test_ride = TestRide.find(params[:id])
+              end
 
-        def test_ride_params
-          params.require(:test_ride).permit(:user_id, :address, :name, :mobile, :email, :request_pick_up, :test_ride_done, :test_ride_confirmed, :bike, :ride_date, :ride_time, :location)
-        end
+              def test_ride_params
+                params.require(:test_ride).permit(:user_id, :address, :name, :mobile, :email, :request_pick_up, :test_ride_done, :test_ride_confirmed, :bike, :ride_date, :ride_time, :location)
+              end
 
-      end
+            end
