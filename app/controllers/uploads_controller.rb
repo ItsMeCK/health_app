9f766785f231811d5d6cffa0ae9b09d71d67b134
anil kumar <anil.kumar@myridz.com>
2016-08-31 16:32:12 +0530

    flash message

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -3,41 +3,49 @@ class UploadsController < ActionController::Base
 
     def import
       Bike.import(params[:file])
+      flash[:success] = "Bikes Uploaded Successfully"
       redirect_to :back
     end
 
   def import_specifications
     Specification.import(params[:file])
+    flash[:success] = "Specifications Uploaded Successfully"
     redirect_to :back
   end
 
   def import_users
     User.import(params[:file])
+    flash[:success] = "Users Uploaded Successfully"
     redirect_to :back
   end
 
   def import_service_history
     ServiceHistory.import(params[:file])
+    flash[:success] = "Service History Uploaded Successfully"
     redirect_to :back
   end
 
   def import_prices
     Pricing.import(params[:file])
+    flash[:success] = "Prices Uploaded Successfully"
     redirect_to :back
   end
 
   def import_key_features
     KeyFeature.import(params[:file])
+    flash[:success] = "Key Features Uploaded Successfully"
     redirect_to :back
   end
 
   def import_colors
     BikeColor.import(params[:file])
+    flash[:success] = "Bike Colors Uploaded Successfully"
     redirect_to :back
   end
 
   def import_service_schedules
     ServiceSchedule.import(params[:file])
+    flash[:success] = "Service Schedules Uploaded Successfully"
     redirect_to :back
   end
 
