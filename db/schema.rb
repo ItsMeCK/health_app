366e8c22bce6506f5ee09d78566aad1745835861
anil kumar <anil.kumar@myridz.com>
2016-09-12 12:11:07 +0530

    image to notification migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829131417) do
+ActiveRecord::Schema.define(version: 20160912061141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -189,13 +189,6 @@ ActiveRecord::Schema.define(version: 20160829131417) do
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
-  create_table "documents", force: :cascade do |t|
-    t.string   "name"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "email_notification_templates", force: :cascade do |t|
     t.text     "content"
     t.string   "title"
@@ -360,6 +353,7 @@ ActiveRecord::Schema.define(version: 20160829131417) do
     t.boolean  "active",     default: true
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.string   "image"
   end
 
   create_table "notifications", force: :cascade do |t|
