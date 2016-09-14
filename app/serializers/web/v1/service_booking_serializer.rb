1952f33f5b31ea7b5a194ddc5eeb369c30a0936f
anil kumar <anil.kumar@myridz.com>
2016-09-14 09:57:48 +0530

    condition for strftime in service book

--- app/serializers/web/v1/service_booking_serializer.rb
+++ app/serializers/web/v1/service_booking_serializer.rb
@@ -1,17 +1,21 @@
 class Web::V1::ServiceBookingSerializer < ActiveModel::Serializer
-  
-  attributes :id, :service_type, :user_id, :my_bike_id, :registration_number, :kms, :service_date, :service_time, :service_station, :comments, :request_pick_up, :status
+	
+	attributes :id, :service_type, :user_id, :my_bike_id, :registration_number, :kms, :service_date, :service_time, :service_station, :comments, :request_pick_up, :status
 
-  def attributes
-  	data = super
-  	data[:service_time] = (data[:service_time]).strftime("%H:%M:%S %p")
-  	if MyBike.exists?(data[:my_bike_id])
-	  	data[:my_bike_name] = (MyBike.find(data[:my_bike_id]).bike if MyBike.exists?(data[:my_bike_id])) || "N/A"
-	  	data[:user_email] = (MyBike.find(data[:my_bike_id]).user.try(:email) if MyBike.exists?(data[:my_bike_id])) || "N/A"
-	  	data[:user_name] = (MyBike.find(data[:my_bike_id]).user.profile.try(:full_name) if MyBike.exists?(data[:my_bike_id])) || "N/A"
-	  	data[:user_mobile] = (MyBike.find(data[:my_bike_id]).user.try(:mobile) if MyBike.exists?(data[:my_bike_id])) || "N/A"
-	  	data
+	def attributes
+		data = super
+		if data[:service_time].present?
+			data[:service_time] = (data[:service_time]).strftime("%H:%M:%S %p")
+		else
+			data[:service_time] = "nul"
+		end
+		if MyBike.exists?(data[:my_bike_id])
+			data[:my_bike_name] = (MyBike.find(data[:my_bike_id]).bike if MyBike.exists?(data[:my_bike_id])) || "N/A"
+			data[:user_email] = (MyBike.find(data[:my_bike_id]).user.try(:email) if MyBike.exists?(data[:my_bike_id])) || "N/A"
+			data[:user_name] = (MyBike.find(data[:my_bike_id]).user.profile.try(:full_name) if MyBike.exists?(data[:my_bike_id])) || "N/A"
+			data[:user_mobile] = (MyBike.find(data[:my_bike_id]).user.try(:mobile) if MyBike.exists?(data[:my_bike_id])) || "N/A"
+			data
+		end
 	end
-  end
 
 end
