c105804eca661d9c86154e11a46524c98c3aba2c
anil kumar <anil.kumar@myridz.com>
2016-09-15 13:03:03 +0530

    my bookings issue code changes

--- app/models/service_booking.rb
+++ app/models/service_booking.rb
@@ -9,19 +9,19 @@ class ServiceBooking < ActiveRecord::Base
 	end
 
 	def user_name
-		if MyBike.exists?(self.my_bike_id)
+		if MyBike.exists?(self.my_bike_id) && MyBike.find_by_id(self.my_bike_id).user.present?
 			MyBike.find_by_id(self.my_bike_id).user.profile.try(:full_name) || "null"	
 		end
 	end
 
 	def user_mobile
-		if MyBike.exists?(self.my_bike_id)
-			MyBike.find_by_id(self.my_bike_id).user.profile.try(:mobile) || "null"	
+		if MyBike.exists?(self.my_bike_id) && MyBike.find_by_id(self.my_bike_id).user.present?
+			  MyBike.find_by_id(self.my_bike_id).user.profile.try(:mobile) || "null"	
 		end
 	end
 
 	def user_email
-		if MyBike.exists?(self.my_bike_id)
+		if MyBike.exists?(self.my_bike_id) && MyBike.find_by_id(self.my_bike_id).user.present?
 			MyBike.find_by_id(self.my_bike_id).user.email || "null"	
 		end
 	end
