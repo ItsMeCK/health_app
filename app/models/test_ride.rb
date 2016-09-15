166402e0a6296de6fecf93edf12995e34ddda0f2
anil kumar <anil.kumar@myridz.com>
2016-09-15 12:19:38 +0530

    changed the code

--- app/models/test_ride.rb
+++ app/models/test_ride.rb
@@ -3,6 +3,7 @@ class TestRide < ActiveRecord::Base
 	belongs_to :user
 
 	def test_ride_booking_notification(n_template, dealer_mail_template, customer_mail_template)
+		logger.info "=======Test Booking Notification Error #{self.inspect}======"
 		template = NotificationTemplate.where(category: n_template).last
 	    Notification.create(recipient: self.user, actor: self.user, action: 'Bookings', notifiable: self, notification_template: template)
 	    UserMailer.test_ride_booking(self, dealer_mail_template).deliver
