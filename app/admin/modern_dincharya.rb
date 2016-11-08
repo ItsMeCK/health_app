95526405f132267568386a1c6db728d9bf62a03d
Chandrakant Shinde <chandrakant.shinde@myridz.com>
2016-11-08 16:20:41 +0530

    Admin Changes for Dincharyas

--- app/admin/modern_dincharya.rb
+++ app/admin/modern_dincharya.rb
@@ -3,6 +3,10 @@ ActiveAdmin.register ModernDincharya do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
+menu :parent => "Dincharya"
+  action_item only: :show  do
+	link_to "New ModernDincharya", action: :new # so link will only be available on show action
+  end
 permit_params :title, :reminder_time, :description
 
 #
